5926c60c9cf9c0dc414a4b6d29234aaa34cd4fb9
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2022-11-23 10:45:17 +0800

    added back default clocks

--- javascript/index.js
+++ javascript/index.js
@@ -2,41 +2,49 @@ function updateTime() {
   //perth//
   function updatePerthTime() {
     let perthElement = document.querySelector("#perth");
-    let perthDateElement = perthElement.querySelector(".date");
-    let perthTimeElement = perthElement.querySelector(".time");
-    let perthTime = moment().tz("Australia/Perth");
-    perthDateElement.innerHTML = moment().format("MMMM Do YYYY");
-    perthTimeElement.innerHTML = `${perthTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    )}`;
+    if (perthElement) {
+      let perthDateElement = perthElement.querySelector(".date");
+      let perthTimeElement = perthElement.querySelector(".time");
+      let perthTime = moment().tz("Australia/Perth");
+      perthDateElement.innerHTML = moment().format("MMMM Do YYYY");
+      perthTimeElement.innerHTML = `${perthTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      )}`;
+    }
   }
   setInterval(updatePerthTime, 1000);
   //auckland//
   function updateAucklandTime() {
     let aucklandElement = document.querySelector("#auckland");
-    let aucklandDateElement = aucklandElement.querySelector(".date");
-    let aucklandTimeElement = aucklandElement.querySelector(".time");
-    let aucklandTime = moment().tz("Pacific/Auckland");
-    aucklandDateElement.innerHTML = moment().format("MMMM Do YYYY");
-    aucklandTimeElement.innerHTML = `${aucklandTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    )}`;
+    if (aucklandElement) {
+      let aucklandDateElement = aucklandElement.querySelector(".date");
+      let aucklandTimeElement = aucklandElement.querySelector(".time");
+      let aucklandTime = moment().tz("Pacific/Auckland");
+      aucklandDateElement.innerHTML = moment().format("MMMM Do YYYY");
+      aucklandTimeElement.innerHTML = `${aucklandTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      )}`;
+    }
   }
   setInterval(updateAucklandTime, 1000);
   //london//
 
   function updateLondonTime() {
     let londonElement = document.querySelector("#london");
-    let londonDateElement = londonElement.querySelector(".date");
-    let londonTimeElement = londonElement.querySelector(".time");
-    let londonTime = moment().tz("Europe/London");
-    londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
-    londonTimeElement.innerHTML = `${londonTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    )}`;
+    if (londonElement) {
+      let londonDateElement = londonElement.querySelector(".date");
+      let londonTimeElement = londonElement.querySelector(".time");
+      let londonTime = moment().tz("Europe/London");
+      londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+      londonTimeElement.innerHTML = `${londonTime.format(
+        "h:mm:ss [<small>]A[</small>]"
+      )}`;
+    }
   }
   setInterval(updateLondonTime, 1000);
 }
+
+updateTime();
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
@@ -54,7 +62,7 @@ function updateCity(event) {
           </div>
           <div class="time">${cityTime.format(
             "h:mm:ss"
-          )}<small>${cityTime.format("A")}</small>            
+          )} <small>   ${cityTime.format("A")}</small>            
             </div>
         </div>`;
 }
